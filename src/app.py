62a2c78dabf398a7ceacf2aea993ecdf78902e8e
Clatt79928 <kevinhong79928@gmail.com>
2024-07-04 10:20:34 +0800

    fix bfs/dfs 2

--- src/app.py
+++ src/app.py
@@ -14,11 +14,12 @@ class App:
         self.clock = pygame.time.Clock()
         self.running = True
         self.state = 'main_menu'
+        self.algorithm_state = ''
         self.load()
 
         self.start_end_checker = 0  
         self.route_found = False
-        self.best_route = None
+        set.mouse_drag =0
 
         # start and end nodes coordinates
         self.start_node_x = None
@@ -26,7 +27,7 @@ class App:
         self.end_node_x = None
         self.end_node_y = None
 
-        self.wall_pos = wall_nodes_coords_list
+        self.wall_pos = wall_nodes_coords_list.copy()
         # wall nodes list (list already includes the coordinates of the borders)
         
         self.bfs_button = Buttons(self,WHITE,338,BUTTON_HEIGHT,200,70,'BFS')
@@ -38,12 +39,14 @@ class App:
         while self.running:
             if self.state == 'main_menu':
                 self.main_menu_events()
-            if self.state == 'show grid':
+            if self.state == 'grid window':
                 self.grid_events()
             if self.state == 'draw S/E' or self.state == 'draw walls':
                 self.draw_nodes()
             if self.state == 'start visualizing':
                 self.execute_search_algorithm()
+            if self.state == 'aftermath':
+                self.reset_or_main_menu()
 
 
         pygame.quit()
@@ -95,11 +98,62 @@ class App:
             pygame.draw.line(self.screen, ALICE, (GS_X, GS_Y + y*self.grid_square_length),
                              (GE_X, GS_Y + y*self.grid_square_length))
     def sketch_grid_buttons(self):
-        # Draw buttons
+        # draw buttons
         self.start_end_node_button.draw_button(STEELBLUE)
         self.wall_node_button.draw_button(STEELBLUE)
         self.reset_button.draw_button(STEELBLUE)
         self.start_button.draw_button(STEELBLUE)
+
+##### checks for state when button is clicked and changes button colour when hovered over.
+    def grid_window_buttons(self, pos, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.start_end_node_button.isOver(pos):
+                self.state = 'draw S/E'
+            elif self.wall_node_button.isOver(pos):
+                self.state = 'draw walls'
+            elif self.reset_button.isOver(pos):
+                self.execute_reset()
+            elif self.start_button.isOver(pos):
+                self.state = 'start visualizing'
+
+        # Get mouse position and check if it is hovering over button
+        if event.type == pygame.MOUSEMOTION:
+            if self.start_end_node_button.isOver(pos):
+                self.start_end_node_button.colour = MINT
+            elif self.wall_node_button.isOver(pos):
+                self.wall_node_button.colour = MINT
+            elif self.reset_button.isOver(pos):
+                self.reset_button.colour = MINT
+            elif self.start_button.isOver(pos):
+                self.start_button.colour = MINT
+            else:
+                self.start_end_node_button.colour, self.wall_node_button.colour, self.reset_button.colour, self.start_button.colour = STEELBLUE, STEELBLUE, STEELBLUE, STEELBLUE
+
+    def grid_button_keep_colour(self):
+        if self.state == 'draw S/E':
+            self.start_end_node_button.colour = MINT
+
+        elif self.state == 'draw walls':
+            self.wall_node_button.colour = MINT
+
+    def execute_reset(self):
+        self.route_found = False
+        self.start_end_checker = 0
+
+        #start and end nodes coordinates
+        self.start_node_x = None
+        self.start_node_y = None
+        self.end_node_x = None
+        self.end_node_y = None
+
+        #reset class attributes
+        self.bfs.bfs_reset()
+
+        # Wall Nodes List (list already includes the coordinates of the borders)
+        self.wall_pos = wall_nodes_coords_list.copy()
+
+        # Switch States
+        self.state = 'grid window'
 
 
     def main_menu_events(self):
@@ -117,13 +171,17 @@ class App:
             pos = pygame.mouse.get_pos()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if self.bfs_button.isOver(pos):
-                    self.state = 'show grid'
+                    self.algorithm_state = 'bfs'
+                    self.state = 'grid window'
                 if self.dfs_button.isOver(pos):
-                    self.state = 'show grid'
+                    self.algorithm_state = 'dfs'
+                    self.state = 'grid window'
                 if self.astar_button.isOver(pos):
-                    self.state = 'show grid'
+                    self.algorithm_state = 'astar'
+                    self.state = 'grid window'
                 if self.dijkstra_button.isOver(pos):
-                    self.state = 'show grid'
+                    self.algorithm_state = 'dijkstra'
+                    self.state = 'grid window'
             #get mouse position and check if it is hovering the button
             if event.type == pygame.MOUSEMOTION:
                 if self.bfs_button.isOver(pos):
@@ -143,6 +201,7 @@ class App:
 #playing state functions
 
     def grid_events(self):
+        #print(len(wall_nodes_coords_list))
         self.sketch_hotbar()
         self.sketch_grid()
         self.sketch_grid_buttons()
@@ -152,36 +211,14 @@ class App:
             if event.type == pygame.QUIT:
                 self.running = False
             pos = pygame.mouse.get_pos()
-            # get mouse position and check if it is clicking button
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if self.start_end_node_button.isOver(pos):
-                    self.state = 'draw S/E'
-                elif self.wall_node_button.isOver(pos):
-                    self.state = 'draw walls'
-                elif self.reset_button.isOver(pos):
-                    print('pressed c')
-                elif self.start_button.isOver(pos):
-                    print('pressed d')
 
-            # get mouse position and check if it is hovering over button
-            if event.type == pygame.MOUSEMOTION:
-                if self.start_end_node_button.isOver(pos):
-                    self.start_end_node_button.colour = MINT
-                elif self.wall_node_button.isOver(pos):
-                    self.wall_node_button.colour = MINT
-                elif self.reset_button.isOver(pos):
-                    self.reset_button.colour = MINT
-                elif self.start_button.isOver(pos):
-                    self.start_button.colour = MINT
-                else:
-                    self.start_end_node_button.colour, self.wall_node_button.colour, self.reset_button.colour, self.start_button.colour = STEELBLUE, STEELBLUE, STEELBLUE, STEELBLUE
+            #helper function
+            self.grid_window_buttons(pos, event)
 
 
 #drawing state functions
-    # - check where the mouse is clicking on grid
-    # - add in feature to draw nodes on grid
-    # - add in feature so that the drawn nodes on grid translate onto text file
     def draw_nodes(self):
+        self.grid_button_keep_colour()
         self.sketch_grid_buttons()
         pygame.display.update()
         pos = pygame.mouse.get_pos()
@@ -190,31 +227,11 @@ class App:
             if event.type == pygame.QUIT:
                 self.running = False
 
-            # get mouse position and check if it is clicking button
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if self.start_end_node_button.isOver(pos):
-                    self.state = 'draw S/E'
-                elif self.wall_node_button.isOver(pos):
-                    self.state = 'draw walls'
-                elif self.reset_button.isOver(pos):
-                    print('Work In Progress')
-                elif self.start_button.isOver(pos):
-                    self.state = 'start visualizing'
+            self.grid_window_buttons(pos, event)
 
-            # Get mouse position and check if it is hovering over button
-            if event.type == pygame.MOUSEMOTION:
-                if self.start_end_node_button.isOver(pos):
-                    self.start_end_node_button.colour = MINT
-                elif self.wall_node_button.isOver(pos):
-                    self.wall_node_button.colour = MINT
-                elif self.reset_button.isOver(pos):
-                    self.reset_button.colour = MINT
-                elif self.start_button.isOver(pos):
-                    self.start_button.colour = MINT
-                else:
-                    self.start_end_node_button.colour, self.wall_node_button.colour, self.reset_button.colour, self.start_button.colour = STEELBLUE, STEELBLUE, STEELBLUE, STEELBLUE
+            
 
-            # checking if mouse is within the boundary of the grid to draw
+            # set boundaries for where mouse position is valid
             if pos[0] > 264 and pos[0] < 1512 and pos[1] > 24 and pos[1] < 744:
                 x_grid_pos = (pos[0] - 264) // 24
                 y_grid_pos = (pos[1] - 24) // 24
@@ -223,6 +240,11 @@ class App:
                 # get mouse position and check if it is clicking button. 
                 # then, draw if clicking
                 if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.mouse_drag = 1
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    self.mouse_drag = 0
+
+                if self.mouse_drag == 1:                    
                     if self.state == 'draw S/E' and self.start_end_checker < 2:
 
                         # choose point color for grid and record the coordinate of start pos
@@ -234,20 +256,24 @@ class App:
                             self.start_end_checker += 1
 
                         # choose point color for grid and record the coordinate of end pos
-                        elif self.start_end_checker == 1:
+                        elif self.start_end_checker == 1 and x_grid_pos + 1 != self.start_node_x and y_grid_pos + 1 != self.start_node_y:
                             node_colour = ROYALBLUE
                             self.end_node_x = x_grid_pos + 1
                             self.end_node_y = y_grid_pos + 1
                             # print(self.end_node_x, self.end_node_y)
                             self.start_end_checker += 1
+                        
+                        else:
+                            continue
 
                         # draw point on grid
                         pygame.draw.rect(self.screen, node_colour, (264 + x_grid_pos * 24, 24 + y_grid_pos * 24, 24, 24), 0)
 
-                    # draw wall Nodes and append wall node coordinates to the wall nodes list
+                    # draw wall nodes and append wall node coordinates to the wall nodes list
                     elif self.state == 'draw walls':
                         pygame.draw.rect(self.screen, BLACK, (264 + x_grid_pos*24, 24 + y_grid_pos*24, 24, 24), 0)
-                        self.wall_pos.append((x_grid_pos + 1, y_grid_pos + 1))
+                        if (x_grid_pos + 1, y_grid_pos + 1) not in self.wall_pos:
+                            self.wall_pos.append((x_grid_pos + 1, y_grid_pos + 1))
                         # print(len(self.wall_pos))
 
 #visualizing state functions
@@ -261,18 +287,51 @@ class App:
         #print(self.end_node_x, self.end_node_y)
 
             # make BFS object
+        if self.algorithm_state == 'bfs':
             self.bfs = BreadthFirst(self.start_node_x, self.start_node_y, self.end_node_x, self.end_node_y, self.wall_pos)
 
             if self.start_node_x or self.end_node_x is not None:
                 while not self.route_found:
                     self.bfs.bfs_execute()
-                    best_route = self.bfs.route
                     self.route_found = True
 
             # make Object for new path
-            while True:
-                self.draw_path = VisualizePath(self.screen, self.start_node_x, self.start_node_y, best_route)
-                self.draw_path.get_path_coords()
-                self.draw_path.draw_path()
-                pygame.display.update()
+            self.draw_path = VisualizePath(self.screen, self.start_node_x, self.start_node_y, self.bfs.route)
+            self.draw_path.get_path_coords()
+            self.draw_path.draw_path()
+            pygame.display.update()
+            self.state = 'aftermath'
 
+        elif self.algorithm_state == 'dfs':
+            pass
+        elif self.algorithm_state == 'astar':
+            pass
+        elif self.algorithm_state == 'dijkstra':
+            pass
+
+
+    def reset_or_main_menu(self):
+        self.sketch_grid_buttons()
+        pygame.display.update()
+
+        pos = pygame.mouse.get_pos()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+
+            if event.type == pygame.MOUSEMOTION:
+                if self.start_end_node_button.isOver(pos):
+                    self.start_end_node_button.colour = MINT
+                elif self.wall_node_button.isOver(pos):
+                    self.wall_node_button.colour = MINT
+                elif self.reset_button.isOver(pos):
+                    self.reset_button.colour = MINT
+                elif self.start_button.isOver(pos):
+                    self.start_button.colour = MINT
+                else:
+                    self.start_end_node_button.colour, self.wall_node_button.colour, self.reset_button.colour, self.start_button.colour = STEELBLUE, STEELBLUE, STEELBLUE, STEELBLUE
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if self.reset_button.isOver(pos):
+                    self.execute_reset()
